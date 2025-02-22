c7312aa753c216cd1b1b7828208b7b06918992f4
a22300788-BenardoIldefonso <a22300788@alunos.ulht.pt>
2025-02-22 20:00:29 +0000

    dinamic, part1

--- faz_e_conta/data_hub/views.py
+++ faz_e_conta/data_hub/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render
-
 from .forms import AlunoForm
 from .models import Aluno
 
-
 def show_students(request):
     data = Aluno.objects.all()
-    return render(request, "show_students.html", {"data": data})
+    head = [field.name for field in Aluno._meta.fields]
+    
+    # Melhorando a criação da lista de dicionários
+    data_dict = list(data.values(*head))
+    
+    return render(request, "show_students.html", {"data": data, "head": head, "data_dict": data_dict})
 
 def insert_student(request):
     if request.method == "POST":
@@ -16,4 +19,5 @@ def insert_student(request):
             return redirect("show_students")
     else:
         form = AlunoForm()
+    
     return render(request, "insert_student.html", {"form": form})
