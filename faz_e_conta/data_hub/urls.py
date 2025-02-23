8ce7b0772fb341a9a12550763c8bd64fc05137f1
a22300788-BenardoIldefonso <a22300788@alunos.ulht.pt>
2025-02-23 23:50:17 +0000

    Show aluno

--- faz_e_conta/data_hub/urls.py
+++ faz_e_conta/data_hub/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.show_students, name='show_students'),
     path('insert/', views.insert_student, name='insert_student'),
+    path('<int:aluno_id>/', views.show_student, name='show_student'),
 ]
--- faz_e_conta/data_hub/views.py
+++ faz_e_conta/data_hub/views.py
@@ -5,11 +5,24 @@ from .models import Aluno
 def show_students(request):
     data = Aluno.objects.all()
     head = [field.name for field in Aluno._meta.fields]
+    #head = ["aluno_id", "nome_proprio", "apelido"]
     
     # Melhorando a criação da lista de dicionários
     data_dict = list(data.values(*head))
     
     return render(request, "show_students.html", {"head": head, "data_dict": data_dict, "id": head[0]})
+
+
+
+
+def show_student(request, aluno_id):
+    data = Aluno.objects.get(aluno_id=aluno_id)
+    head = [field.name for field in Aluno._meta.fields]
+    #head = ["aluno_id", "nome_proprio", "apelido"]
+    data_dict = {head[i]: data.__dict__[head[i]] for i in range(1, len(head))}
+    
+    return render(request, "show_student.html", {"head": head, "data_dict": data_dict, "data":data, "id": head[0]})
+
 
 def insert_student(request):
     if request.method == "POST":
