08c7d8226eb50d1dc87bcb814e0d89bc59300048
brunoxx0037 <96127673+brunoxx0037@users.noreply.github.com>
2025-02-25 18:52:34 +0000

    Add Dummy_data test for aluno

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
 from util import db
+import os
 
 def main():
     # Get connection to DB or create DB if it does not exist
     conn = db.get_connection()
 
     # Create a table
-    db.drop_table('aluno', conn)
-    db.create_table_aluno(conn)
+    # db.drop_table('aluno', conn)
+    # db.create_table_aluno(conn)
 
     # Insert (dummy) data to table
-    db.insert_to_aluno({"nome_proprio": "João", "apelido": "Silva"}, conn)
-    db.insert_to_aluno({"nome_proprio": "Maria", "apelido": "Santos"}, conn)
-    db.insert_to_aluno({"nome_proprio": "Pedro", "apelido": "Almeida"}, conn)
+    # db.insert_to_aluno({"nome_proprio": "João", "apelido": "Silva"}, conn)
+    # db.insert_to_aluno({"nome_proprio": "Maria", "apelido": "Santos"}, conn)
+    # db.insert_to_aluno({"nome_proprio": "Pedro", "apelido": "Almeida"}, conn)
 	
+    # Load dummy data from CSV files
+    source_path = os.path.join("resources", "dummy_data")
+    print(f"Loading dummy data from {source_path}...")
+    db.load_dummy_data(source_path, truncate=True, conn=conn)
+
     # Get all data from table
     query = "SELECT * FROM aluno;"
     rows = db.fetchall(query, conn)
