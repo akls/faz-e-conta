527290b5e1c83ff7d470592a83a8a1eac8ec37f8
a22300788-BenardoIldefonso <a22300788@alunos.ulht.pt>
2025-03-15 12:14:49 +0000

    changes

--- run.py
+++ run.py
@@ -1,14 +1,24 @@
 from util import db
-import os
 
 def main():
     # Get connection to DB or create DB if it does not exist
     conn = db.get_connection()
 
-    source_path = os.path.join("resources", "dummy_data")
-    print(f"Loading dummy data from {source_path}...")
-    db.load_dummy_data(source_path, truncate=True, conn=conn)
+    # Create a table
+    db.drop_table('aluno', conn)
+    db.create_table_aluno(conn)
 
+    # Insert (dummy) data to table
+    db.insert_to_aluno({"nome_proprio": "João", "apelido": "Silva"}, conn)
+    db.insert_to_aluno({"nome_proprio": "Maria", "apelido": "Santos"}, conn)
+    db.insert_to_aluno({"nome_proprio": "Pedro", "apelido": "Almeida"}, conn)
+	
+    # Get all data from table
+    query = "SELECT * FROM aluno;"
+    rows = db.fetchall(query, conn)
+    conn.close()
+    for row in rows:
+        print(row)
 
 if __name__ == "__main__":
     main()
