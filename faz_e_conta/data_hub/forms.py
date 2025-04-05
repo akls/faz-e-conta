38eae49f8cbb99bbf381ec0ddc11db718c1e1bfa
a22300788-BenardoIldefonso <a22300788@alunos.ulht.pt>
2025-04-06 00:31:10 +0100

    after migrate

--- faz_e_conta/data_hub/forms.py
+++ faz_e_conta/data_hub/forms.py
@@ -5,7 +5,7 @@ from .widgets import *
 class AlunoForm(forms.ModelForm):
     class Meta:
         model = Aluno
-        fields = ['aluno_id', 'nome_proprio', 'apelido', 'processo', 'data_admissao', 'data_ultima_renovacao', 'data_nascimento', 'documento', 'numero_documento', 'data_validade', 'niss', 'nif', 'morada', 'codigo_postal', 'concelho', 'fregesia', 'escolaridade_anterior', 'motivo_admissao', 'cuidados_especias']
+        fields = ['aluno_id', 'nome_proprio', 'apelido', 'archive_flag', 'processo', 'data_admissao', 'data_ultima_renovacao', 'data_nascimento', 'documento', 'numero_documento', 'data_validade', 'niss', 'nif', 'morada', 'codigo_postal', 'concelho', 'fregesia', 'escolaridade_anterior', 'motivo_admissao', 'cuidados_especias', 'sala_id']
 
         # Adiciona atributos aos campos do formulário
         widgets = Aluno_widget()
@@ -21,7 +21,7 @@ class Responsavel_educativoForm(forms.ModelForm):
 class Aluno_saidaForm(forms.ModelForm):
     class Meta:
         model = AlunoSaida
-        fields = ['aluno_saida_id', 'aluno_id', 'hora_entrada', 'hora_saida', 'autorizacao_sair', 'escolaridade']
+        fields = ['aluno_saida_id', 'aluno_id', 'hora_entrada', 'hora_saida', 'autorizacao_sair', 'valencia']
 
         # Adiciona atributos aos campos do formulário
         widgets = Aluno_saida_widget()
@@ -34,27 +34,75 @@ class VacinacaoForm(forms.ModelForm):
         # Adiciona atributos aos campos do formulário
         widgets = Vacinacao_widget()
 
-class DespesaForm(forms.ModelForm):
+class Despesa_fixaForm(forms.ModelForm):
     class Meta:
-        model = Despesa
-        fields = ['despesa_id', 'categoria', 'valor', 'descricao', 'data']
+        model = DespesaFixa
+        fields = ['despesa_fixa_id', 'produto', 'valor', 'data', 'fatura', 'pagamento']
 
         # Adiciona atributos aos campos do formulário
-        widgets = Despesa_widget()
+        widgets = Despesa_fixa_widget()
+
+class Despesas_variavelForm(forms.ModelForm):
+    class Meta:
+        model = DespesasVariavel
+        fields = ['despesas_variavel_id', 'produto', 'valor', 'data', 'fatura', 'pagamento']
+
+        # Adiciona atributos aos campos do formulário
+        widgets = Despesas_variavel_widget()
 
 class SalarioForm(forms.ModelForm):
     class Meta:
         model = Salario
-        fields = ['salario_id', 'responsavel_educativo_id', 'valor', 'descricao', 'data_pagamento', 'subsidio_tipo', 'subsidio_valor']
+        fields = ['salario_id', 'valor', 'descricao', 'data_pagamento', 'subsidio_tipo', 'subsidio_valor']
 
         # Adiciona atributos aos campos do formulário
         widgets = Salario_widget()
 
-class FiliacaoForm(forms.ModelForm):
+class Link_filiacaoForm(forms.ModelForm):
     class Meta:
-        model = Filiacao
-        fields = ['filiacao_id', 'aluno_id', 'responsavel_educativo_id', 'filiacao_responsavel']
+        model = LinkFiliacao
+        fields = ['aluno_id', 'responsavel_educativo_id', 'type', 'encarr_educacao', 'link_filiacao_id']
 
         # Adiciona atributos aos campos do formulário
-        widgets = Filiacao_widget()
+        widgets = Link_filiacao_widget()
+
+class SalaForm(forms.ModelForm):
+    class Meta:
+        model = Sala
+        fields = ['sala_id', 'sala_nome', 'sala_local', 'sala_valencia', 'funcionario_id']
+
+        # Adiciona atributos aos campos do formulário
+        widgets = Sala_widget()
+
+class Mensalidade_alunoForm(forms.ModelForm):
+    class Meta:
+        model = MensalidadeAluno
+        fields = ['ma_id', 'aluno_id', 'ano_letivo', 'periodo_inicio', 'periodo_fim', 'mensalidade_calc', 'mensalidade_retific', 'mensalidade_paga', 'data_pagamento', 'modo_pagamento', 'acordo']
+
+        # Adiciona atributos aos campos do formulário
+        widgets = Mensalidade_aluno_widget()
+
+class Aluno_financasForm(forms.ModelForm):
+    class Meta:
+        model = AlunoFinancas
+        fields = ['aluno_financas_id', 'ano_letivo', 'aluno_id', 'data', 'agregado', 'rendim_líquido', 'despesa_anual', 'irs', 'tax_soc', 'tax_impos', 'renda', 'med_transp', 'medicacao', 'outros']
+
+        # Adiciona atributos aos campos do formulário
+        widgets = Aluno_financas_widget()
+
+class Aluno_finacas_calcForm(forms.ModelForm):
+    class Meta:
+        model = AlunoFinacasCalc
+        fields = ['sala_id', 'nome', 'local', 'valencia', 'func_id']
+
+        # Adiciona atributos aos campos do formulário
+        widgets = Aluno_finacas_calc_widget()
+
+class FuncionarioForm(forms.ModelForm):
+    class Meta:
+        model = Funcionario
+        fields = ['funcionario_id', 'nome_proprio', 'apelido', 'data_nascimento', 'tipo_documento_identificacao', 'numero_documento_identificacao', 'data_validade', 'niss', 'nif', 'morada', 'codigo_postal', 'concelho', 'freguesia_residencia', 'contacto_telefonico', 'email', 'funcao', 'salario', 'escalao_profissional', 'ativo']
+
+        # Adiciona atributos aos campos do formulário
+        widgets = Funcionario_widget()
 
--- faz_e_conta/data_hub/widgets.py
+++ faz_e_conta/data_hub/widgets.py
@@ -1,7 +1,6 @@
 from django import forms
 
 def Aluno_widget():
-    # Adiciona atributos aos campos do formulário
     widgets = {
         'data_admissao': forms.DateInput(attrs={'type': 'date'}),
         'data_ultima_renovacao': forms.DateInput(attrs={'type': 'date'}),
@@ -10,40 +9,85 @@ def Aluno_widget():
     }
     return widgets
 
-        
 def Responsavel_educativo_widget():
     widgets = {
-        
+        'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
+        'data_validade': forms.DateInput(attrs={'type': 'date'}),
     }
     return widgets
 
 def Aluno_saida_widget():
     widgets = {
-        
+        'hora_entrada': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        'hora_saida': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
     }
     return widgets
 
 def Vacinacao_widget():
     widgets = {
-        
-    }
-    return widgets
-
-def Despesa_widget():
-    widgets = {
-        
+        'data_vacina': forms.DateInput(attrs={'type': 'date'}),
     }
     return widgets
 
 def Salario_widget():
     widgets = {
-        
+        'data_pagamento': forms.DateInput(attrs={'type': 'date'}),
     }
     return widgets
 
 def Filiacao_widget():
     widgets = {
-        
+        'encarr_educacao': forms.CheckboxInput(),
     }
     return widgets
 
+def Mensalidade_aluno_widget():
+    widgets = {
+        'periodo_inicio': forms.DateInput(attrs={'type': 'date'}),
+        'periodo_fim': forms.DateInput(attrs={'type': 'date'}),
+        'data_pagamento': forms.DateInput(attrs={'type': 'date'}),
+    }
+    return widgets
+
+def Aluno_financas_widget():
+    widgets = {
+        'data': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+    }
+    return widgets
+
+def Funcionario_widget():
+    widgets = {
+        'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
+        'data_validade': forms.DateInput(attrs={'type': 'date'}),
+    }
+    return widgets
+
+def Sala_widget():
+    widgets = {
+        'sala_local': forms.TextInput(attrs={'type': 'text'}),
+    }
+    return widgets
+
+def Link_filiacao_widget():
+    widgets = {
+        'encarr_educacao': forms.CheckboxInput(),
+    }
+    return widgets
+
+def Aluno_finacas_calc_widget():
+    widgets = {
+        'local': forms.TextInput(attrs={'type': 'text'}),
+    }
+    return widgets
+
+def Despesa_fixa_widget():
+    widgets = {
+        'data': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+    }
+    return widgets
+
+def Despesas_variavel_widget():
+    widgets = {
+        'data': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+    }
+    return widgets
