31351b9643311c1036fe593327c2c8ee6888ac35
Nuno-Matos-22306389 <nuno.sfmatos@gmail.com>
2026-03-22 19:32:36 +0000

    template show aluno_financas e view do mesmo para ter mensalidade aluno

--- faz_e_conta/data_hub/views.py
+++ faz_e_conta/data_hub/views.py
@@ -5,7 +5,7 @@ from .auto_gen_form_views import *
 from .auto_gen_id_views import *
 from django.db.models import Q  # Import Q for dynamic filtering
 from django.db.models import F, Value, CharField
-from django.db.models.functions import Concat
+from django.db.models.functions import Concat, Cast
 
 
 def starter_page(request):
@@ -54,25 +54,65 @@ def show_students(request):
     return render(request, "show_students.html", context)
 
 def show_financas(request):
-    query = request.GET.get("q", "")  # Get search query from the URL
 
-    # Base queryset
-    data = AlunoFinancas.objects.all()
+    data_financas = AlunoFinancas.objects.annotate(
+        Ano_letivo=F('ano_letivo'),
+        Despesa_anual=F('despesa_anual'),
+        Rendimento_líquido=F('rendim_líquido'),
+        Nome_Completo=Concat(
+'aluno_id__nome_proprio',
+            Value(' '),
+            'aluno_id__apelido',
+            output_field=CharField()
+        ),
+        Documento=F('aluno_id__numero_documento')
+    )
+    data_mensalidade = MensalidadeAluno.objects.annotate(
+        Data=Concat(
+            'ano',
+            Value('/'),
+            'mes',
+            output_field=CharField()
+        ),
+        Calculado=F('mensalidade_calc'),
+        Retificado=F('mensalidade_retific'),
+        Quantidade_Paga = F('mensalidade_paga'),
+        Data_Pagamento= Cast('data_pagamento', CharField()),
+        Modo_Pagamento=F('modo_pagamento'),
+        Serviço=F('programa_ss'),
+        Acordo = F('acordo')
+    ).order_by('ano','mes')
 
-    # Apply search filter by student ID
-    if query:
-        data = data.filter(aluno_id__aluno_id__icontains=query)
 
-    # Define the fields to display
-    head = ["ano_letivo", "despesa_anual", "rendim_líquido", "aluno_id__nome_proprio", "aluno_id__apelido", "aluno_id__numero_documento"]
-    data_dict = list(data.values(*head))
+    head_financas = \
+        [
+        "Ano_letivo",
+        "Despesa_anual",
+        "Rendimento_líquido",
+        "Nome_Completo",
+        "Documento"
+         ]
+    head_mensalidade = \
+        [
+        "Data",
+        "Calculado",
+        "Retificado",
+        "Quantidade_Paga",
+        "Data_Pagamento",
+        "Modo_Pagamento",
+        "Serviço",
+        "Acordo"
+        ]
 
-    # Render the template with context
+
+    data_financas = list(data_financas.values(*head_financas))
+    data_mensalidade = list(data_mensalidade.values(*head_mensalidade))
+
     context = {
-        "head": head,
-        "data_dict": data_dict,
-        "id": "aluno_id__nome_proprio",  # Use student name as identifier
-        "query": query,
+        "head_financas": head_financas,
+        "data_financas": data_financas,
+        "head_mensalidade": head_mensalidade,
+        "data_mensalidade": data_mensalidade,
     }
     return render(request, "show_aluno_financas.html", context)
 
