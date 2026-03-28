e901b5ede248b6955588caf4353444a759346c1a
rodri <a22302691@alunos.ulht.pt>
2026-03-28 23:50:47 +0000

    All required fields in the edit forms for students and guardians are now highlighted in red.

--- faz_e_conta/static/js/forms.js
+++ faz_e_conta/static/js/forms.js
@@ -1,18 +1,22 @@
-// Pegar elementos do DOM
-const form = document.getElementById("form")
-const requiredInputs = form.querySelectorAll("input[required], select[required], textarea[required]")
+document.addEventListener("DOMContentLoaded", () => {
+    // Pegar elementos do DOM
+    const form = document.getElementById("form")
+    const requiredInputs = form.querySelectorAll("input[required], select[required], textarea[required]")
 
 
-
-
-// Adicionar eventos aos campos required
-requiredInputs.forEach(campo =>{
-    campo.classList.add("error");
-    campo.addEventListener("input", () =>{
-        if (campo.value){
-            campo.classList.remove("error");
-        }else{
+    // Adicionar eventos aos campos required
+    requiredInputs.forEach(campo =>{
+        if (!campo.value){
             campo.classList.add("error");
+        }else{
+            campo.classList.remove("error");
         }
+        campo.addEventListener("input", () =>{
+            if (campo.value){
+                campo.classList.remove("error");
+            }else{
+                campo.classList.add("error");
+            }
+        })
     })
-})
+});
